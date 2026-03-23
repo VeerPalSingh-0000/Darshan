49e0d6182cf4993d2bebe990c6197596820400b4
Veer Pal Singh <sveerpal7676@gmail.com>
2026-03-23 23:51:31 +0530

    try4

--- src/Pages/SanskritGuide.jsx
+++ src/Pages/SanskritGuide.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import {
   SpeakerWaveIcon,
